bc811f69b3988ec78b43df7037427b70327d84d7
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-17 14:14:24 -0300

    Atualizar o Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -11,11 +11,10 @@ import openfl.events.Event;
 
 class Main extends Sprite
 {
-	var gameWidth:Int = 1280;
+	var gameWidth:Int  = 1280;
 	var gameHeight:Int = 720;
 	var initialState:Class<FlxState> = TitleState;
-	var zoom:Float = -1;
-	var framerate:Int = 60;
+	var framerate:Int  = 60;
 	var skipSplash:Bool = true;
 	var startFullscreen:Bool = false;
 	public static var fpsVar:FPS;
@@ -30,39 +29,21 @@ class Main extends Sprite
 		super();
 
 		if (stage != null)
-		{
 			init();
-		}
 		else
-		{
 			addEventListener(Event.ADDED_TO_STAGE, init);
-		}
 	}
 
 	private function init(?E:Event):Void
 	{
 		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
 			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
 
 		setupGame();
 	}
 
 	private function setupGame():Void
 	{
-		var stageWidth:Int = Lib.current.stage.stageWidth;
-		var stageHeight:Int = Lib.current.stage.stageHeight;
-
-		if (zoom == -1)
-		{
-			var ratioX:Float = stageWidth / gameWidth;
-			var ratioY:Float = stageHeight / gameHeight;
-			zoom = Math.min(ratioX, ratioY);
-			gameWidth = Math.ceil(stageWidth / zoom);
-			gameHeight = Math.ceil(stageHeight / zoom);
-		}
-
 		#if !debug
 		initialState = TitleState;
 		#end
@@ -71,19 +52,17 @@ class Main extends Sprite
 		Paths.getModFolders();
 		#end
 
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		if (fpsVar != null)
-		{
 			fpsVar.visible = ClientPrefs.showFPS;
-		}
 		#end
 
 		#if (html5 || mobile)
-		FlxG.autoPause = false;
+		FlxG.autoPause    = false;
 		FlxG.mouse.visible = false;
 		#end
 
