61ee2f56ea0215a406f7d7e2b4a80dff231ef19f
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-17 13:11:14 -0300

    Atualizar o Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -11,16 +11,14 @@ import openfl.events.Event;
 
 class Main extends Sprite
 {
-	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 60; // How many frames per second the game should run at.
-	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	var gameWidth:Int = 1280;
+	var gameHeight:Int = 720;
+	var initialState:Class<FlxState> = TitleState;
+	var zoom:Float = -1;
+	var framerate:Int = 60;
+	var skipSplash:Bool = true;
+	var startFullscreen:Bool = false;
 	public static var fpsVar:FPS;
-
-	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
 	{
@@ -69,20 +67,39 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 
+		#if desktop
 		Paths.getModFolders();
+		#end
+
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
-		if(fpsVar != null) {
+		if (fpsVar != null)
+		{
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
 		#end
 
-		#if html5
+		#if (html5 || mobile)
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
 		#end
+
+		#if android
+		lime.system.System.allowScreenTimeout = false;
+		#end
+
+		#if mobile
+		stage.addEventListener(Event.RESIZE, onStageResize);
+		#end
 	}
+
+	#if mobile
+	private function onStageResize(E:Event):Void
+	{
+		FlxG.resizeGame(stage.stageWidth, stage.stageHeight);
+	}
+	#end
 }
