402dac77ccc41b7fa244a371623e72cbac78d8fc
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-18 17:39:20 -0300

    Implement external storage permission request for Android
    Added external storage permission request for Android.

--- source/Main.hx
+++ source/Main.hx
@@ -8,14 +8,17 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
+#if android
+import lime.system.JNI;
+#end
 
 class Main extends Sprite
 {
-	var gameWidth:Int  = 1280;
-	var gameHeight:Int = 720;
+	var gameWidth:Int        = 1280;
+	var gameHeight:Int       = 720;
 	var initialState:Class<FlxState> = TitleState;
-	var framerate:Int  = 60;
-	var skipSplash:Bool = true;
+	var framerate:Int        = 60;
+	var skipSplash:Bool      = true;
 	var startFullscreen:Bool = false;
 	public static var fpsVar:FPS;
 
@@ -62,18 +65,59 @@ class Main extends Sprite
 		#end
 
 		#if (html5 || mobile)
-		FlxG.autoPause    = false;
+		FlxG.autoPause     = false;
 		FlxG.mouse.visible = false;
 		#end
 
 		#if android
 		lime.system.System.allowScreenTimeout = false;
+		requestExternalStoragePermission();
 		#end
 
 		#if mobile
 		stage.addEventListener(Event.RESIZE, onStageResize);
 		#end
 	}
+
+	#if android
+	private function requestExternalStoragePermission():Void
+	{
+		var getActivity = JNI.createStaticMethod(
+			"org/haxe/lime/GameActivity",
+			"getInstance",
+			"()Lorg/haxe/lime/GameActivity;"
+		);
+		var activity = getActivity();
+
+		var sdkVersion:Int = JNI.createStaticField(
+			"android/os/Build$VERSION",
+			"SDK_INT",
+			"I"
+		).get();
+
+		var perms:Array<String> = [
+			"android.permission.READ_EXTERNAL_STORAGE",
+			"android.permission.WRITE_EXTERNAL_STORAGE"
+		];
+
+		if (sdkVersion >= 30)
+			perms.push("android.permission.MANAGE_EXTERNAL_STORAGE");
+
+		if (sdkVersion >= 33)
+		{
+			perms.push("android.permission.READ_MEDIA_IMAGES");
+			perms.push("android.permission.READ_MEDIA_AUDIO");
+			perms.push("android.permission.READ_MEDIA_VIDEO");
+		}
+
+		var requestPermissions = JNI.createMemberMethod(
+			"android/app/Activity",
+			"requestPermissions",
+			"([Ljava/lang/String;I)V"
+		);
+		requestPermissions(activity, perms, 0);
+	}
+	#end
 
 	#if mobile
 	private function onStageResize(E:Event):Void
