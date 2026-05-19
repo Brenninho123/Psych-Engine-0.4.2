0f0810ab2b00e455e50d4173b1491c897762fe5b
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-19 14:00:29 -0300

    Atualizar o Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -3,11 +3,18 @@ package;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
-import openfl.Assets;
 import openfl.Lib;
 import openfl.display.FPS;
+import openfl.display.Shape;
 import openfl.display.Sprite;
 import openfl.events.Event;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+import openfl.text.TextFieldAutoSize;
+#if android
+import sys.thread.Thread;
+import sys.thread.Mutex;
+#end
 
 class Main extends Sprite
 {
@@ -27,18 +34,14 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-
-		if (stage != null)
-			init();
-		else
-			addEventListener(Event.ADDED_TO_STAGE, init);
+		if (stage != null) init();
+		else addEventListener(Event.ADDED_TO_STAGE, init);
 	}
 
 	private function init(?E:Event):Void
 	{
 		if (hasEventListener(Event.ADDED_TO_STAGE))
 			removeEventListener(Event.ADDED_TO_STAGE, init);
-
 		setupGame();
 	}
 
@@ -46,9 +49,125 @@ class Main extends Sprite
 	{
 		#if android
 		Storage.requestPermissions();
-		Storage.init();
+		showLoadingScreen();
+		#else
+		finishSetup();
 		#end
+	}
 
+	#if android
+	private function showLoadingScreen():Void
+	{
+		var sw:Float = stage.stageWidth;
+		var sh:Float = stage.stageHeight;
+
+		var bg = new Shape();
+		bg.graphics.beginFill(0x000000);
+		bg.graphics.drawRect(0, 0, sw, sh);
+		bg.graphics.endFill();
+		addChild(bg);
+
+		var titleFmt = new TextFormat("_sans", 32, 0xFFFFFF, true);
+		var title    = new TextField();
+		title.defaultTextFormat = titleFmt;
+		title.text              = "FNF: Psych Engine 0.4.2";
+		title.autoSize          = TextFieldAutoSize.CENTER;
+		title.selectable        = false;
+		title.x                 = (sw - title.textWidth) / 2;
+		title.y                 = sh * 0.3;
+		addChild(title);
+
+		var subtitleFmt = new TextFormat("_sans", 18, 0xAAAAAA);
+		var subtitle    = new TextField();
+		subtitle.defaultTextFormat = subtitleFmt;
+		subtitle.text              = "Extracting files, please wait...";
+		subtitle.autoSize          = TextFieldAutoSize.CENTER;
+		subtitle.selectable        = false;
+		subtitle.x                 = (sw - subtitle.textWidth) / 2;
+		subtitle.y                 = title.y + 50;
+		addChild(subtitle);
+
+		var barW:Float = sw * 0.65;
+		var barH:Float = 22;
+		var barX:Float = (sw - barW) / 2;
+		var barY:Float = sh * 0.55;
+
+		var barBg = new Shape();
+		barBg.graphics.beginFill(0x333333);
+		barBg.graphics.drawRoundRect(barX, barY, barW, barH, 11, 11);
+		barBg.graphics.endFill();
+		addChild(barBg);
+
+		var barFill = new Shape();
+		addChild(barFill);
+
+		var statusFmt = new TextFormat("_sans", 14, 0x888888);
+		var statusTF  = new TextField();
+		statusTF.defaultTextFormat = statusFmt;
+		statusTF.width             = sw;
+		statusTF.autoSize          = TextFieldAutoSize.CENTER;
+		statusTF.selectable        = false;
+		statusTF.x                 = 0;
+		statusTF.y                 = barY + barH + 10;
+		addChild(statusTF);
+
+		var mutex    = new Mutex();
+		var progress = {v: 0.0};
+		var msg      = {v: "Preparing..."};
+		var done     = {v: false};
+
+		Thread.create(function()
+		{
+			Storage.init(function(p:Float, m:String)
+			{
+				mutex.acquire();
+				progress.v = p;
+				msg.v      = m;
+				mutex.release();
+			});
+			mutex.acquire();
+			done.v = true;
+			mutex.release();
+		});
+
+		var onFrame:Event->Void = null;
+		onFrame = function(e:Event)
+		{
+			mutex.acquire();
+			var p:Float  = progress.v;
+			var m:String = msg.v;
+			var d:Bool   = done.v;
+			mutex.release();
+
+			barFill.graphics.clear();
+			if (p > 0)
+			{
+				barFill.graphics.beginFill(0xFF2E6EE3);
+				barFill.graphics.drawRoundRect(barX, barY, barW * p, barH, 11, 11);
+				barFill.graphics.endFill();
+			}
+
+			statusTF.text = m;
+
+			if (d)
+			{
+				stage.removeEventListener(Event.ENTER_FRAME, onFrame);
+				removeChild(barFill);
+				removeChild(barBg);
+				removeChild(statusTF);
+				removeChild(subtitle);
+				removeChild(title);
+				removeChild(bg);
+				finishSetup();
+			}
+		};
+
+		stage.addEventListener(Event.ENTER_FRAME, onFrame);
+	}
+	#end
+
+	private function finishSetup():Void
+	{
 		#if !debug
 		initialState = TitleState;
 		#end
